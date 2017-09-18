c2124f5c2109c8ef7fc24aeb936b8756a876836d
hepplerj <jason.heppler@gmail.com>
2017-09-18 11:19:43 -0500

    More data cleanup; fixing coordinates; removing junk strings.

--- data-raw/scripts.R
+++ data-raw/scripts.R
@@ -1,14 +1,25 @@
 library(tidyverse)
+library(lubridate)
 superfunds <- read_csv('data-raw/superfund_sites.csv')
 
+# Clean up dates
 # To be handled better by read_csv once spreadsheet is cleaned up
 superfunds$proposed <- gsub("–","",superfunds$proposed)
 superfunds$listed <- gsub("–","",superfunds$listed)
 superfunds$construction_completed <- gsub("–","",superfunds$construction_completed)
 superfunds$partially_deleted <- gsub("–","",superfunds$partially_deleted)
 superfunds$deleted <- gsub("–","",superfunds$deleted)
-
-# Fix up dates
 superfunds[,8:12] <- lapply(superfunds[,8:12], function(x) mdy(x))
 
-devtools::use_data(superfunds)
+# Clean up empty data
+superfunds$report <- gsub("–","",superfunds$report)
+superfunds$latitude <- gsub("–","",superfunds$latitude)
+superfunds$latitude <- gsub("\\s*\\[[^\\)]+\\]","",superfunds$latitude)
+superfunds$longitude <- gsub("–","",superfunds$longitude)
+superfunds$longitude <- gsub("\\s*\\[[^\\)]+\\]","",superfunds$longitude)
+
+# Fix coordinates
+superfunds$longitude <- as.numeric(gsub(".","",superfunds$longitude, fixed = TRUE))
+superfunds$latitude <- as.numeric(gsub(".","",superfunds$latitude, fixed = TRUE))
+
+devtools::use_data(superfunds, overwrite = TRUE)
