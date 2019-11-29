e3510db80a4d1919396c336cb06d2c6c2adffce6
hepplerj <jason.heppler@gmail.com>
2019-11-28 21:22:29 -0600

    Data cleanup.

--- data-raw/scripts.R
+++ data-raw/scripts.R
@@ -1,26 +1,33 @@
 suppressPackageStartupMessages(library(tidyverse))
+suppressPackageStartupMessages(library(tabulizer))
 suppressPackageStartupMessages(library(lubridate))
-superfunds <- read_csv('data-raw/superfund_sites.csv')
 
-# Clean up dates
-# To be handled better by read_csv once spreadsheet is cleaned up
-superfunds$proposed <- gsub("–","",superfunds$proposed)
-superfunds$listed <- gsub("–","",superfunds$listed)
-superfunds$construction_completed <- gsub("–","",superfunds$construction_completed)
-superfunds$partially_deleted <- gsub("–","",superfunds$partially_deleted)
-superfunds$deleted <- gsub("–","",superfunds$deleted)
-superfunds[,8:12] <- lapply(superfunds[,8:12], function(x) mdy(x))
+# Location of pdf file
+location <- 'https://semspub.epa.gov/work/HQ/201184.pdf'
 
-# Clean up empty data
-superfunds$report <- gsub("–","",superfunds$report)
-superfunds$reason <- gsub("\\s*\\[[^\\)]+\\]","",superfunds$reason)
-superfunds$latitude <- gsub("–","",superfunds$latitude)
-superfunds$latitude <- gsub("\\s*\\[[^\\)]+\\]","",superfunds$latitude)
-superfunds$longitude <- gsub("–","",superfunds$longitude)
-superfunds$longitude <- gsub("\\s*\\[[^\\)]+\\]","",superfunds$longitude)
+# Extract the table
+out <- extract_tables(location)
+superfunds <- do.call(rbind, out[-length(out)])
+rm(out)
+rm(location)
 
-# Fix coordinates
-superfunds$longitude <- as.numeric(superfunds$longitude)
-superfunds$latitude <- as.numeric(superfunds$latitude)
+# table headers get extracted as rows with bad formatting. Dump them.
+superfunds <- as.data.frame(superfunds[-1,])
 
-devtools::use_data(superfunds, overwrite = TRUE)
+# Column names
+headers <- c('region', 'state', 'site_name', 'site_id', 'epa_id', 'address',
+             'city', 'zip', 'county', 'ff', 'latitude','longitude','superfunds_date')
+
+# Apply custom column names
+names(superfunds) <- headers
+
+superfunds <- superfunds %>%
+  # Convert date columns to date objects
+  mutate_each(list(~ as.Date(., format='%m/%d/%y')), final_date)
+write_csv(superfunds, "data-raw/superfunds.csv")
+
+# After some by-hand cleaning of the data, read back in the changes.
+superfunds <- read_csv("data-raw/superfunds_cleaned.csv")
+names(superfunds) <- headers
+
+usethis::use_data(superfunds, overwrite = TRUE)
