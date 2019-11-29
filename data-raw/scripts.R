3087725f796aeac85611cd9a2041d4e83d1ab926
hepplerj <jason.heppler@gmail.com>
2019-11-29 11:23:31 -0600

    Updated data files from EPA.

--- data-raw/scripts.R
+++ data-raw/scripts.R
@@ -1,33 +1,51 @@
-suppressPackageStartupMessages(library(tidyverse))
-suppressPackageStartupMessages(library(tabulizer))
+suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(lubridate))
+suppressPackageStartupMessages(library(readxl))
+suppressPackageStartupMessages(library(tabulizer))
+
+superfunds <- readxl::read_xlsx("data-raw/201186.xlsx", skip = 1)
+headers <- c('site_name', 'epa_id', 'city', 'county', 'state', 'zipcode', 'region',
+             'npl_status', 'superfund_agreement', 'federal_facility', 'op_unit_no',
+             'seq_id','decision_type','completion_date', 'fiscal_year',
+             'media', 'contaminant')
+names(superfunds) <- headers
+
+superfunds <- superfunds %>%
+  mutate_at(c(7,11, 12), as.numeric) %>%
+  mutate(fiscal_year = as.Date(fiscal_year, format = "%Y")) %>%
+  mutate(fiscal_year = lubridate::year(fiscal_year))
+
+# Extract lat/lon from PDF tables
+# -------------------------------
 
 # Location of pdf file
 location <- 'https://semspub.epa.gov/work/HQ/201184.pdf'
 
 # Extract the table
 out <- extract_tables(location)
-superfunds <- do.call(rbind, out[-length(out)])
+superfunds_match <- do.call(rbind, out[-length(out)])
 rm(out)
 rm(location)
 
 # table headers get extracted as rows with bad formatting. Dump them.
-superfunds <- as.data.frame(superfunds[-1,])
+superfunds_match <- as.data.frame(superfunds_match[-1,])
 
 # Column names
-headers <- c('region', 'state', 'site_name', 'site_id', 'epa_id', 'address',
+headers_match <- c('region', 'state', 'site_name', 'site_id', 'epa_id', 'address',
              'city', 'zip', 'county', 'ff', 'latitude','longitude','final_date')
 
-# Apply custom column names
-names(superfunds) <- headers
+# Apply custom column names.
+names(superfunds_match) <- headers_match
 
-superfunds <- superfunds %>%
-  # Convert date columns to date objects
-  mutate_each(list(~ as.Date(., format='%m/%d/%y')), final_date)
-write_csv(superfunds, "data-raw/superfunds.csv")
+# Match EPA ID from superfunds and superfunds_match to pull in lat/long
+# coordinates and addresses.
+superfunds_latlon <- superfunds_match %>%
+  select(epa_id, address, latitude, longitude) %>%
+  mutate_at(c("latitude","longitude"), list(~ as.numeric(levels(.))[.]))
 
-# After some by-hand cleaning of the data, read back in the changes.
-superfunds <- read_csv("data-raw/superfunds_cleaned.csv")
-names(superfunds) <- headers
+superfunds <- left_join(superfunds, superfunds_latlon, by = "epa_id")
+rm(superfunds_latlon)
+rm(superfunds_match)
+
 
 usethis::use_data(superfunds, overwrite = TRUE)
